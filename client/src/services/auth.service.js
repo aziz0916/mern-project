e603320f05a2a24bae6c438cd7a7edcc5f9dc28e
aziz0916 <aziz0916@yahoo.com.tw>
2022-12-14 22:45:26 +0800

    Set API URL

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://project9-mern.herokuapp.com/api/user";
 
 class AuthService {
   login(email, password) {
