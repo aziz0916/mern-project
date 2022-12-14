b930dc64c9b7d830163901afcef5a1b781df53e7
aziz0916 <aziz0916@yahoo.com.tw>
2022-12-14 22:33:26 +0800

    Make changes for deployment

--- server.js
+++ server.js
@@ -8,27 +8,36 @@ const courseRoute = require('./routes').course
 const passport = require('passport')
 require('./config/passport')(passport)
 const cors = require('cors')
+const path = require('path')
+const port = process.env.PORT || 8080 // process.env.PORT是Heroku自動動態設定
 
 // 連結MongoDB
 mongoose
-.connect('mongodb://localhost:27017/mernDB')
-.then(() => {
-  console.log("連結到mongodb...");
-})
-.catch((e) => {
-  console.log(e)
-})
+  .connect(process.env.MONGODB_CONNECTION)
+  .then(() => {
+    console.log("連結到mongodb...");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use('/api/user', authRoute)
 // course route應該被jwt保護
 // 如果request header內部沒有jwt，則request就會被視為是unauthorized
 app.use('/api/courses', passport.authenticate('jwt', { session: false }), courseRoute)
 
-app.listen(8080, () => {
-  console.log('後端伺服器聆聽在port 8080...')
-})
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', "build", "index.html"));
+  })
+}
+
+app.listen(port, () => {
+  console.log("後端伺服器聆聽在port 8080...");
+});
